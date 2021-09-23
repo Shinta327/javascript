fc7efdfd3d714edc9353839531f7b948aa844bef
ShintaKanayama <shinta327tuba@gmail.com>
2021-09-23 23:09:38 +0900

    [Finish]7

---
+++ 7/app.js
@@ -0,0 +1,40 @@
+// $(function(){
+//   $('.bg1').on('click', function(){
+//     $('.bg1').slideUp();
+//   });
+// });
+
+// $(function(){
+//   $('.bg2').on('click', function(){
+//     $('.bg2').slideUp();
+//   });
+// });
+
+// $(function(){
+//   $('.bg3').on('click', function(){
+//     $('.bg3').slideUp();
+//   });
+// });
+
+// $(function(){
+//   $('.bg4').on('click', function(){
+//     $('.bg4').slideUp();
+//   });
+// });
+
+
+// 上記のコードをthisを用いて記述
+// this = クリックされた要素、ここではbox1のクラスを持つ4つのdivのうち、クリックされた要素のみスライドアップされる
+// $(function(){
+//   $('.box1').on('click', function(){
+//     $(this).slideUp();
+//   });
+// });
+
+
+// children
+$(function(){
+  $('button').on('click', function(){
+    $('ul').children().css('color','red');
+  });
+});
