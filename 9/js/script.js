5072c433c4ca4bfa1c8e8c7caaa8632478393dee
ShintaKanayama <shinta327tuba@gmail.com>
2021-09-27 23:12:05 +0900

    [review]9

--- 9/js/script.js
+++ 9/js/script.js
@@ -1,3 +1,18 @@
+// $(function(){
+//   $('#back a').on('click', function(event){
+//     $('body, html').animate({
+//       scrollTop:0
+//     }, 800);
+//     event.preventDefault();
+//   });
+// });
+
+
+// 復習
+// back a でbackIDの子要素であるaタグを指定している
+// clickでクリック時にイベント発火を指定、範囲をbody,htmlつまりページ全体を指定している
+// animateでスクロール位置とスピードを指定する
+// event.preventDefaultはaタグを押すとページトップに戻るというHTMLの設定を無効化している
 $(function(){
   $('#back a').on('click', function(event){
     $('body, html').animate({
